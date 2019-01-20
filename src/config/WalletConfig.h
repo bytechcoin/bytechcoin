ee85d480a24a1cd2dc29fb3949aed58b7ebe95fb
bytechcoin <bahadir.togay.yilmaz@gmail.com>
2019-01-20 12:45:47 +0300

    Update of Wallet

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -11,7 +11,7 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string addressPrefix = "ByTC";
+    const std::string addressPrefix = "bYTC";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
     const std::string ticker = "BYTC";
