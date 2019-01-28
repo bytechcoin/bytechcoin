5c7ad02290f1c61bcca6eb5adc3df7779b707898
BytechCoin <46326675+bytechcoin@users.noreply.github.com>
2019-01-28 15:32:27 +0300

    Seed Nodes updates

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -250,7 +250,7 @@ const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 
 const char* const SEED_NODES[] = {
   "178.32.178.27:11126", //YetkinSeed1
+  "5.39.6.152:11126", //YetkinSeed2  
   "37.148.210.136:11126", //NatroSeed2
-  "54.234.163.240:11126", //AWS-Seed3
 };
 } // CryptoNote
