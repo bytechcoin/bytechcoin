cf1e6631508af21800f646d53c9d1148642ee64b
BytechCoin <46326675+bytechcoin@users.noreply.github.com>
2019-02-05 13:23:06 +0300

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -37,7 +37,7 @@ namespace WalletConfig
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
 
     /* Where can your users contact you for support? E.g. discord */
-    const std::string contactLink = "http://chat.bytechcoin.com";
+    const std::string contactLink = "https://discordapp.com/invite/4BBg8q";
 
 
     /* The number of decimals your coin has */
