dfc6e44f32ab93f6cb12b59ea7cdcc1e4a627fe4
BytechCoin <46326675+bytechcoin@users.noreply.github.com>
2019-02-06 15:56:30 +0300

    Update CryptoNoteCheckpoints.h

--- src/config/CryptoNoteCheckpoints.h
+++ src/config/CryptoNoteCheckpoints.h
@@ -26,5 +26,15 @@ struct CheckpointData {
   const char* blockId;
 };
 
-const std::initializer_list<CheckpointData> CHECKPOINTS = {};
+const std::initializer_list<CheckpointData> CHECKPOINTS = {
+{       0, "d00cfddac0a42e63531b39e3ee60238c15f3f779fddac85d6aa11c18fffc4257"},
+{       2, "ca7be6ceaa111d3097393fd5b6a433e1ed3a6baafb1666a77b9500d05f9d1200"},
+{     100, "e5c5213669a83a364e73c3d83bdc76d8c266d9c4af76d645e2d5c9e3590d2355"},
+{     200, "f7987986c66c0884adae1c88a605dae6e1ed13693816a77108a97b254ae06c4c"},
+{     300, "a74486f0cae78fafcf663259018ffcc0b8cb56872bfb49861065f3e8b57f78ea"},
+{     400, "3afe59ccfc1c856b13ccddcdcc7659167dcfdc537fcb2320b26316d38d3880b8"},
+{     500, "609073be16417ff5f3b96bb87ca8cc5b4f9f6ba7bab33fd826a4058d08eabd1b"},
+{     600, "f7987986c66c0884adae1c88a605dae6e1ed13693816a77108a97b254ae06c4c"},
+{     700, "564ed490c92d9372eb8e3649582496704aadbb69679359547911ef38d8483d58"}
+};
 }
