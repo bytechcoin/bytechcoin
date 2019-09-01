fac6292843836103f85d08b9e5c9405b43d0f010
bytechcoin <bahadir.togay.yilmaz@gmail.com>
2019-09-01 20:47:55 +0300

    Remove build directory

--- build/external/cryptopp/CMakeFiles/dynamic.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dynamic"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dynamic.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/external/cryptopp/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/togayyilmaz/Desktop/bytechcoin-0.14/external/cryptopp
-# Build directory: /home/togayyilmaz/Desktop/bytechcoin-0.14/build/external/cryptopp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(build_cryptest "/usr/local/lib/python2.7/dist-packages/cmake/data/bin/cmake" "--build" "/home/togayyilmaz/Desktop/bytechcoin-0.14/build" "--target" "cryptest")
-add_test(cryptest "/home/togayyilmaz/Desktop/bytechcoin-0.14/build/external/cryptopp/cryptest.exe" "v")
-set_tests_properties(cryptest PROPERTIES  DEPENDS "build_cryptest")
